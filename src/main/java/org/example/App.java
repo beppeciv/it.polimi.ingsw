f39ca860e0181e76dfe756b6fa3d84e13dd4d985
beppe <giuseppe.civaschi@mail.polimi.it>
2021-02-28 10:43:54 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int a=3; int b=8;
+        System.out.println(a+b);
+    }
+}
